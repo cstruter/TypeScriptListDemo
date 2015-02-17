466061d5e1c1c87976d5f718c9967c4da222190e
Christoff Truter <christoff@cstruter.com>
2015-02-17 21:35:51 +0200

    Cleanups

--- SimpleListDemo/Models/User.js
+++
@@ -1,9 +0,0 @@
-var User = (function () {
-    function User(FirstName, LastName, Username) {
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.Username = Username;
-    }
-    return User;
-})();
-//# sourceMappingURL=User.js.map
